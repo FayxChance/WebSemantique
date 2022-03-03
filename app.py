3a7af28f0c89443cdee4ca1f274397ce850d719c
Nicolas.C <comcomnico@gmail.com>
2022-03-03 20:52:51 +0100

    done

--- app.py
+++ app.py
@@ -1,23 +1,55 @@
+import os
+import networkx as nx
+
 from flask import Flask, render_template
 from pyvis.network import Network
-import networkx as nx
-import os
+
+from sparql import SparQL
 
 template_dir = os.path.abspath('./templates')
 
 app = Flask(__name__, template_folder=template_dir)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
-@app.route('/visualization')
-def visualization():
+@app.route('/date/<date>')
+def date(date):
+    reference = "film_name"
+    research_word = date
+    spql = SparQL(reference, research_word)
+    spql.execute()
+    
     G = nx.Graph()
-    G.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    G.add_edges_from([(1, 2), (1, 3), (1, 4), (5, 7), (5, 8), (5, 9), (5, 10), (6, 7), (6, 8), (6, 9), (8, 9), (8, 10), (9, 10)])
-    nt = Network(height="50vh", width="50vw", bgcolor="#222222", font_color="white", heading="Network")
+    G.add_node(research_word)
+    
+    for item in spql.data[reference]:
+        G.add_node(item.movie)
+        G.add_edge(research_word, item.movie)
+    
+    nt = Network(bgcolor="#222222", font_color="white")
+    nt.from_nx(G)
+    nt.save_graph("./templates/network.html")
+    return render_template('visualization.html')
+
+
+@app.route('/movie/<movie>')
+def visualization(movie):
+    reference = "actors_name"
+    research_word = movie
+    spql = SparQL(reference, research_word)
+    spql.execute()
+    
+    G = nx.Graph()
+    G.add_node(research_word)
+    
+    for item in spql.data[reference]:
+        G.add_node(item.name)
+        G.add_edge(research_word, item.name)
+        
+    nt = Network(bgcolor="#222222", font_color="white")
     nt.from_nx(G)
     nt.save_graph("./templates/network.html")
     return render_template('visualization.html')
